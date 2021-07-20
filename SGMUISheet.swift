17bb0ec6a57a25f1539bbbcb17125be881b406f1
andy <andy>
2021-07-20 22:35:21 +0800

    提交

---
+++ SGMUISheet.swift
@@ -0,0 +1,14 @@
+//
+//  SGMUISheet.swift
+//  dxyy_ios
+//
+//  Created by 苏贵明 on 2021/7/20.
+//
+
+import UIKit
+
+class SGMUISheet: NSObject {
+    class func test(){
+        print("打印成功")
+    }
+}
